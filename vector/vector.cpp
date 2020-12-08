ae6884942ceb6a34bb29c06d7015264135b585a2
Alexander <jangl@i.ua>
2020-12-09 01:46:54 +0200

    iterate over the vector

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -4,6 +4,17 @@
 int main()
 {
     std::vector<int> vect{ 7, 9, 1, 7, 8, 0, 9, 1, 8 };
+	for (size_t i = 0; i < vect.size(); ++i)
+	{
+		size_t count = 0;
+		for (size_t j = 0; j < vect.size(); ++j)
+		{
+			if (vect[i] == vect[j])
+			{
+				count++;
+			}
+		}
+	}
 }
 
 
